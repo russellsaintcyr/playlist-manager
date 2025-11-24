5fc1cab75ec4a9557fab2bd1a857086a1b9389fe
1111x <russellsaintcyr@gmail.com>
2025-11-24 11:58:04 -0500

    Configurable size icons

--- src/app/components/ratings-buttons/ratings-buttons.component.ts
+++ src/app/components/ratings-buttons/ratings-buttons.component.ts
@@ -14,6 +14,7 @@ import { FirebaseService } from '../../services/firebase.service';
 export class RatingsButtonsComponent implements OnInit {
   @Input() track!: Track;
   @Input() selectedPlaylist: any;
+  @Input() fontSize: string = '55px';
   
   private firebaseService = inject(FirebaseService);
   private ratings: Array<Rating> = [];
